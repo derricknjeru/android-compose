41a1009738c569c7d66598110262c4a87defa80c
Derrick <derricknjeru5@gmail.com>
2021-03-09 17:14:48 +0300

    Defined composable functions Used and styled columns to improve layout Styled the App with Material Design principles

--- JetpackComposeBasics/app/src/main/java/com/derrick/jetpackcomposebasics/MainActivity.kt
+++ JetpackComposeBasics/app/src/main/java/com/derrick/jetpackcomposebasics/MainActivity.kt
@@ -3,18 +3,62 @@ package com.derrick.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import com.derrick.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Text(text = "Derrick")
+            NewStory()
         }
     }
+
+    @Composable
+    private fun NewStory(){
+        MaterialTheme {
+            val typography = MaterialTheme.typography
+            Column(modifier = Modifier.padding(16.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.header),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(180.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop,
+
+                    )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text("A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text("Davenport, California", style = typography.body2)
+                Text("December 2018", style = typography.body2)
+            }
+        }
+
+    }
+
+    @Preview
+    @Composable
+    private fun DefaultPreview(){
+      NewStory()
+    }
+
 }
